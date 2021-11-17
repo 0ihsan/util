14a04e4fd2bf2f3c6aa29928025bfabfce6cbc84
ihsan <ihsanl@pm.me>
2021-11-17 19:55:08 +0300

    add splitn

---
+++ splitn.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+main(int x,char** v){if(x>1){
+
+	char l[16384];int n=atoi(v[1]);
+	while((fgets(l,16384,stdin))!=NULL){
+		strtok(l,"\n");
+		for(x=0;x<strlen(l)/n+1;x++) printf("%.*s\t",n,&l[x*n]);
+		putchar(10);
+	}
+	return 0;
+
+}else{fprintf(stderr,"split \033[31m<number>\033[0m\n");return 1;}}
